35d14639f8f66e16352a261b47a3a4ef135538eb
afsalms715 <afsalms715@gmail.com>
2022-06-29 12:21:12 +0300

    set focus to todo input field on munting and deleting object

--- src/App.js
+++ src/App.js
@@ -1,30 +1,33 @@
 
 import './App.css';
-import {useState} from "react"
+import {useState,useRef} from "react"
 
 function App() {
   const[todo,setTodo]=useState('')
   const[todos,setTodos]=useState([])
+  const todoInput=useRef()
   return (
     <div className="App">
       <h1>TODO</h1>
       <input onChange={(e)=>{
         setTodo(e.target.value)
-      }} type="text"/>
+      }} type="text" ref={todoInput}/>
       <button onClick={()=>{
         setTodos([...todos,{id:Date.now(),text:todo}])
+        todoInput.current.focus();
       }}>Add</button>
       {
         todos.map((obj1)=>{
           return(
           <diV>
-            <h4>{obj1.text}</h4>
-            <button onClick={()=>{
+            <h4 className='element-inline'>{obj1.text}</h4>
+            <button className='element-inline' onClick={()=>{
               setTodos(todos.filter((obj2)=>{
                 if(obj1.id===obj2.id){
                  obj2=null
                }
-               return obj2}))                
+               return obj2}))
+               todoInput.current.focus();                
             }
           }
             >delete</button>
