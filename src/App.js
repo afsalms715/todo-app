2a90bc33150d5dae30513d561d406a78df3ce42b
afsalms715 <afsalms715@gmail.com>
2022-06-29 15:45:50 +0300

    add some styles

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ function App() {
   const todoInput=useRef()
   return (
     <div className="App">
-      <h1>TODO</h1>
-      <input onChange={(e)=>{
+      <h1 style={{color:'white'}}>TODO</h1>
+      <input className='text-field form-control element-inline' onChange={(e)=>{
         setTodo(e.target.value)
       }} type="text" ref={todoInput}/>
-      <button onClick={()=>{
+      <button className="btn btn-primary element-inline" onClick={()=>{
         setTodos([...todos,{id:Date.now(),text:todo}])
         todoInput.current.focus();
       }}>Add</button>
       {
         todos.map((obj1)=>{
           return(
-          <diV>
-            <h4 className='element-inline'>{obj1.text}</h4>
-            <button className='element-inline' onClick={()=>{
+          <div className='todo-block'>
+            <h4 style={{color:'white'}} className='element-inline'>{obj1.text}</h4>
+            <button  className='element-inline btn btn-warning' onClick={()=>{
               setTodos(todos.filter((obj2)=>{
                 if(obj1.id===obj2.id){
                  obj2=null
@@ -30,8 +30,8 @@ function App() {
                todoInput.current.focus();                
             }
           }
-            >delete</button>
-          </diV>
+            >DONE</button>
+          </div>
           )
         })
       }
